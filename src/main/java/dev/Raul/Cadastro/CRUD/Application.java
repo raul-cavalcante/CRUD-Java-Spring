4e2f0ec8ff8a3f8187f158a50928b6367d22da70
Raul <raulcavalcante.a@gmail.com>
2026-04-06 11:56:28 -0300

    build: Conecta o bd para a aplicacao

--- src/main/java/dev/Raul/Cadastro/CRUD/Application.java
+++ src/main/java/dev/Raul/Cadastro/CRUD/Application.java
@@ -1,7 +1,10 @@
 package dev.Raul.Cadastro.CRUD;
 
+import org.h2.server.web.JakartaWebServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +13,11 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Bean
+	public ServletRegistrationBean<JakartaWebServlet> h2ConsoleServlet() {
+		ServletRegistrationBean<JakartaWebServlet> bean =
+				new ServletRegistrationBean<>(new JakartaWebServlet(), "/h2-console/*");
+		bean.addInitParameter("webAllowOthers", "true");
+		return bean;
+	}
 }
