3629c13c83798c6d936c89915a61e848a6c2f333
Raul <raulcavalcante.a@gmail.com>
2026-04-07 16:42:37 -0300

    feat: Altera a rota do controller para listar os ninjas e injeta a dependencia so SERVICE e do REPOSITORY

--- src/main/java/dev/Raul/Cadastro/CRUD/Ninjas/NinjaController.java
+++ src/main/java/dev/Raul/Cadastro/CRUD/Ninjas/NinjaController.java
@@ -1,27 +1,40 @@
 package dev.Raul.Cadastro.CRUD.Ninjas;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 
 @RestController
 @RequestMapping("/ninjas")
 public class NinjaController {
+
+
+    private NinjaService ninjaService;
+
+    public NinjaController(NinjaService ninjaService) {
+        this.ninjaService = ninjaService;
+    }
+
 
     @GetMapping("/boasvindas")
     public String boasVindas() {
         return "Esta é minha primeira menssagem nessa rota";
     }
 
+
     //Adicionar Ninja (Create)
 
     @PostMapping("/criar")
     public String criarNinja() {
+
         return "Ninja criado";
     }
 
     //Mostrar todos os ninjas (Read)
     @GetMapping("/listar")
-    public String mostrarTodosNinjas() {
-        return "Ninjas";
+    public List<NinjaModel> listarNinjas() {
+
+        return ninjaService.listarNinjas();
     }
 
     //Mostrar Ninja por id (Read)
--- src/main/java/dev/Raul/Cadastro/CRUD/Ninjas/NinjaService.java
+++ src/main/java/dev/Raul/Cadastro/CRUD/Ninjas/NinjaService.java
@@ -1,4 +1,25 @@
 package dev.Raul.Cadastro.CRUD.Ninjas;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+
+@Service
 public class NinjaService {
+
+
+    private NinjaRepository ninjarepository;
+
+    public NinjaService(NinjaRepository ninjarepository) {
+        this.ninjarepository = ninjarepository;
+    }
+
+
+    //Listar todos os ninjas
+    public List<NinjaModel> listarNinjas() {
+        return ninjarepository.findAll();
+    }
+
+
+
 }
