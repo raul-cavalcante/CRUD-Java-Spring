5a5e1e3369db9ce9d547d39fbbb7d2f34b5910d3
Raul <raulcavalcante.a@gmail.com>
2026-04-08 17:25:28 -0300

    fix: Inclui a coluna de rank na entidade, mas jja estava incluida na migration

--- src/main/java/dev/Raul/Cadastro/CRUD/Ninjas/NinjaController.java
+++ src/main/java/dev/Raul/Cadastro/CRUD/Ninjas/NinjaController.java
@@ -26,7 +26,7 @@ public class NinjaController {
     //Adicionar Ninja (Create)
 
     @PostMapping("/criar")
-    public NinjaModel criarNinja(@RequestBody NinjaModel ninja) {
+    public NinjaDTO criarNinja(@RequestBody NinjaDTO ninja) {
 
         return ninjaService.criarNinja(ninja);
     }
--- src/main/java/dev/Raul/Cadastro/CRUD/Ninjas/NinjaMapper.java
+++ src/main/java/dev/Raul/Cadastro/CRUD/Ninjas/NinjaMapper.java
@@ -1,4 +1,32 @@
 package dev.Raul.Cadastro.CRUD.Ninjas;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class NinjaMapper {
+
+    public NinjaModel map(NinjaDTO ninjaDTO) {
+        NinjaModel ninjaModel = new NinjaModel();
+        ninjaModel.setId(ninjaDTO.getId());
+        ninjaModel.setNome(ninjaDTO.getNome());
+        ninjaModel.setEmail(ninjaDTO.getEmail());
+        ninjaModel.setImgUrl(ninjaDTO.getImgUrl());
+        ninjaModel.setIdade(ninjaDTO.getIdade());
+        ninjaModel.setRank(ninjaDTO.getRank());
+        ninjaModel.setMissoes(ninjaDTO.getMissoes());
+        return ninjaModel;
+    }
+
+    public NinjaDTO map(NinjaModel ninjaModel) {
+        NinjaDTO ninjaDTO = new NinjaDTO();
+        ninjaDTO.setNome(ninjaModel.getNome());
+        ninjaDTO.setEmail(ninjaModel.getEmail());
+        ninjaDTO.setImgUrl(ninjaModel.getImgUrl());
+        ninjaDTO.setIdade(ninjaModel.getIdade());
+        ninjaDTO.setRank(ninjaModel.getRank());
+        ninjaDTO.setId(ninjaModel.getId());
+        ninjaDTO.setMissoes(ninjaModel.getMissoes());
+        return ninjaDTO;
+    }
+
 }
